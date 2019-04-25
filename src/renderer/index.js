75e7294129e60358fe0deb06e9ac88f99dcc0277
jay51 <nenojaafar3@gmail.com>
2019-04-25 04:33:42 -0500

    init project

---
+++ src/renderer/index.js
@@ -0,0 +1,36 @@
+// Initial welcome page. Delete the following line to remove it.
+"use strict";
+
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div id="pomodoro-app">
+        <div id="container">
+          <div id="timer">
+            <div id="time">
+              <span id="minutes">25</span>
+              <span id="colon">:</span>
+              <span id="seconds">00</span>
+            </div>
+            <div id="filler" />
+          </div>
+
+          <div id="buttons">
+            <button id="work">Work</button>
+            <button id="shortBreak">Short Break</button>
+            <button id="longBreak">Long Break</button>
+            <button id="stop">Stop</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+const app = document.getElementById("app");
+ReactDOM.render(<App />, app);
