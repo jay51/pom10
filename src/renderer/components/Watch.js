f45a25de6d89b08827671fea17406260b2e4b711
jay51 <nenojaafar3@gmail.com>
2019-04-26 16:50:57 -0400

    fixed styles & added most features

--- src/renderer/components/Watch.js
+++ src/renderer/components/Watch.js
@@ -3,14 +3,19 @@ import React, { Component } from "react";
 export default class StopWatch extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
       running: false,
       secs: 0,
-      mins: 1,
+      mins: 25,
+      currentTime: 25
     };
+
     this.onTick = this.onTick.bind(this);
     this.handleClick = this.handleClick.bind(this);
     this.onReset = this.onReset.bind(this);
+    this.shortBreack = this.shortBreack.bind(this);
+    this.longBreack = this.longBreack.bind(this);
   }
 
   componentDidMount() {
@@ -37,18 +42,26 @@ export default class StopWatch extends Component {
           this.setState({ secs: secs - 1 });
           return;
         }
-        else this.setState({ mins: mins - 1, secs: 60 });
+        else this.setState({ mins: mins - 1, secs: 59 });
       }
     }
 
   }
 
   handleClick() {
-    this.setState({ running: !this.state.running, previouseTime: Date.now() });
+    this.setState({ running: !this.state.running });
   }
 
   onReset() {
-    this.setState({ secs: 0, mins: 1 });
+    this.setState({ secs: 0, mins: this.state.currentTime });
+  }
+
+  longBreack() {
+    this.setState({ secs: 0, mins: 10, currentTime: 10 });
+  }
+
+  shortBreack() {
+    this.setState({ secs: 0, mins: 5, currentTime: 5 });
   }
 
   render() {
@@ -56,15 +69,23 @@ export default class StopWatch extends Component {
 
     return (
       <div>
+        <div id="timer">
+          <div id="time">
+            <span id="minutes">{mins > 9 ? mins : "0" + mins}</span>
+            <span id="colon">:</span>
+            <span id="seconds">{secs > 9 ? secs : "0" + secs}</span>
+          </div>
+          <div id="filler" />
+        </div>
 
-        <div className="time">{mins > 9 ? mins : "0" + mins}:{secs > 9 ? secs : "0" + secs}</div>
-
-
-        <button onClick={this.handleClick}>
-          {running ? "Stop" : "Start"}
-        </button>
-
-        <button onClick={this.onReset}>Reset</button>
+        <div id="buttons">
+          <button id="work" onClick={this.handleClick}>
+            {running ? "Stop" : "Start"}
+          </button>
+          <button id="shortBreak" onClick={this.shortBreack}>Break <strong>-</strong></button>
+          <button id="longBreak" onClick={this.longBreack}>Break <strong>+</strong></button>
+          <button id="stop" onClick={this.onReset}>Reset</button>
+        </div>
       </div>
     );
   }
@@ -75,3 +96,4 @@ export default class StopWatch extends Component {
 // Done - Make it stop and start as it's now
 // Done - Make it count down instead of up
 // Done - Make it count down to 0 then decremant the mins
+// - Make the cericle itself a stop/start button
--- src/renderer/index.js
+++ src/renderer/index.js
@@ -11,23 +11,10 @@ class App extends Component {
   render() {
     return (
       <div id="pomodoro-app">
-        <Watch />
+        {/* <Watch /> */}
         <div id="container">
-          <div id="timer">
-            <div id="time">
-              <span id="minutes">25</span>
-              <span id="colon">:</span>
-              <span id="seconds">00</span>
-            </div>
-            <div id="filler" />
-          </div>
 
-          <div id="buttons">
-            <button id="work">Work</button>
-            <button id="shortBreak">Short Break</button>
-            <button id="longBreak">Long Break</button>
-            <button id="stop">Stop</button>
-          </div>
+          <Watch />
         </div>
       </div>
     );
